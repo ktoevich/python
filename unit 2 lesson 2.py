4933a04d538eda7ecfe5a877dea72f1953fdcb2b
ktoevich <sherzoddikk@gmail.com>
2025-10-08 23:31:04 +0500

    Update unit 2 lesson 2.py

--- unit 2 lesson 2.py
+++ unit 2 lesson 2.py
@@ -1,12 +1,12 @@
-import requests
-params = {
+import requests # импорт модуля
+params = { # создание множества
     'name' : 'Шерзод',
     'city' : 'Душанбе'
 }
-headers = {
+headers = { # множество для юсер агента
     'User-Agent' : 'agentusera'
 }
-response = requests.get('https://httpbin.org/get', params=params, headers=headers).json()
-print(response['name'])
-print(response['city'])
-print(response['User-Agent'])
+response = requests.get('https://httpbin.org/get', params=params, headers=headers).json() # получение данных как json 
+print(response['name']) # вывод имени
+print(response['city']) # вывод города
+print(response['User-Agent']) # вывод юсер агента
