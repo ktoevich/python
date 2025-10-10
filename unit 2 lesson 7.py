a12148b3b9b647818ce918dbd8c2538661ffd54d
ktoevich <sherzoddikk@gmail.com>
2025-10-11 01:04:55 +0500

    Add files via upload

---
+++ unit 2 lesson 7.py
@@ -0,0 +1,10 @@
+import telebot #импортируем библиотеку
+bot = telebot.TeleBot(token='8012377575:AAEGhciG9u7icMEvx5zITW9lUImsIL7aP-w') #берем токен бота
+
+@bot.message_handler(commands=['echo']) # проверка на команду эхо
+def main(message): #ответ на эхо
+    @bot.message_handler() #берем любое сообщение
+    def info(message): #функция для ответа
+        bot.reply_to(message, message.text) #ответ бота на сообщение пользователя
+
+bot.polling(none_stop = True) #бесконечная работа
