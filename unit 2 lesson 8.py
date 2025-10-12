06a7268ee4ec5c7c48fabb56986df38ee1af1bc8
felliks-c <feliksnekot@gmail.com>
2025-10-12 21:46:32 +0500

    asyncio checked!

--- unit 2 lesson 8.py
+++ unit 2 lesson 8.py
@@ -1,5 +1,6 @@
 import asyncio # для асинхронности
 
+
 async def task1(stop): # функция асинхронности
     await asyncio.sleep(stop) # время ожидания
     print("task 1") # вывод названия
@@ -15,3 +16,37 @@ async def task3(stop): # функция асинхронности
 async def main(): # функция для соеденения всех функций
     await asyncio.gather(task1(1), task2(1), task3(1))
 asyncio.run(main()) # вывод
+
+
+"""
+Check this code to explain how asyncio works on long time tasks:
+
+
+import asyncio # для асинхронности
+import time
+
+
+async def task1(stop): # функция асинхронности
+    await asyncio.sleep(stop) # время ожидания
+    time.sleep(2)
+    print("task 1") # вывод названия
+
+async def task2(stop): # функция асинхронности
+    await asyncio.sleep(stop) # время ожидания
+    time.sleep(3)
+    print("task 2") # вывод названия
+
+async def task3(stop): # функция асинхронности
+    await asyncio.sleep(stop) # время ожидания
+    time.sleep(4)
+    print("task 3") # вывод названия
+
+async def main(): # функция для соеденения всех функций
+    await asyncio.gather(task1(1), task2(1), task3(1))
+asyncio.run(main()) # вывод
+
+
+
+
+Olso u can read about "time" lybrary if u aint know it
+"""
