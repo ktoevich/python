5ec818f10937fe67d19bd010613746563d6f0920
ktoevich <sherzoddikk@gmail.com>
2025-10-12 21:07:15 +0500

    Update unit 2 lesson 2.py

--- unit 2 lesson 2.py
+++ unit 2 lesson 2.py
@@ -7,8 +7,9 @@ headers = { # множество для юсер агента
     'User-Agent' : 'agentusera'
 }
 response = requests.get('https://httpbin.org/get', params=params, headers=headers).json() # получение данных как json 
-print(response['name']) # вывод имени
-print(response['city']) # вывод города
-print(response['User-Agent']) # вывод юсер агента
+print(response['args']['name']) # вывод имени
+print(response['args']['city']) # вывод города
+print(response['args']['User-Agent']) # вывод юсер агента
+
 
 # Оно не работает потому что ты не правильно обращаешься к аргументами пропуская ключь первого уровня. нужно было написать что то на подобии response['args']['name'] и response['args']['city'] и response['headers']['User-Agent']
