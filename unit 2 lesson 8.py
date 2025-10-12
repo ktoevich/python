16c4f2db9fbc5b482f03e45687defe02b97d3dcf
ktoevich <sherzoddikk@gmail.com>
2025-10-12 20:49:07 +0500

    Add files via upload

---
+++ unit 2 lesson 8.py
@@ -0,0 +1,17 @@
+import asyncio # для асинхронности
+
+async def task1(stop): # функция асинхронности
+    await asyncio.sleep(stop) # время ожидания
+    print("task 1") # вывод названия
+
+async def task2(stop): # функция асинхронности
+    await asyncio.sleep(stop) # время ожидания
+    print("task 2") # вывод названия
+
+async def task3(stop): # функция асинхронности
+    await asyncio.sleep(stop) # время ожидания
+    print("task 3") # вывод названия
+
+async def main(): # функция для соеденения всех функций
+    await asyncio.gather(task1(1), task2(1), task3(1))
+asyncio.run(main()) # вывод
